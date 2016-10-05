c6c2327f8517b5f13f439827e08e312a6760ebb6
lfnoleto <lucasnoleto18@gmail.com>
2016-10-05 17:03:21 -0300

    Commit do meu eclipse lucas ferreira noleto

--- Prj_Financas/src/controller/Conta.java
+++ Prj_Financas/src/controller/Conta.java
@@ -3,4 +3,5 @@ package controller;
 public class Conta {
   // Teste da minha casa agora!
   // Teste de alteração direto no GitHub - Beto Lima.
+ // Teste de alteraçao direto do eclipse - lucas ferreira noleto
 }
