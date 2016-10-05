bb1e3081d72311f61dc9c3fe6415ee1213d04996
taffarel55 <mauruciotaffarel@gmail.com>
2016-10-05 17:23:32 -0300

    Teste de commit do Eclipse - Mauricio

--- Prj_Financas/src/controller/Conta.java
+++ Prj_Financas/src/controller/Conta.java
@@ -1,7 +1,7 @@
 package controller;
 
 public class Conta {
-  // Teste da minha casa agora!
+  // Teste da commit direto d Eclipse - Mauricio Taffarel
   // Teste de alteração direto no GitHub - Beto Lima.
   // Teste de commit direto do Eclipse - Lukaz Oliveira (TheMaskked)
 }
