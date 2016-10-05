76ab2f72fed8f35f9570f6836cd849e5c98b686e
Lukaz <lukasgtx7@gmail.com>
2016-10-05 17:14:30 -0300

    Teste de commit do Eclipse - Lukaz

--- Prj_Financas/src/controller/Conta.java
+++ Prj_Financas/src/controller/Conta.java
@@ -3,4 +3,5 @@ package controller;
 public class Conta {
   // Teste da minha casa agora!
   // Teste de alteração direto no GitHub - Beto Lima.
+  // Teste de commit direto do Eclipse - Lukaz Oliveira (TheMaskked)
 }
